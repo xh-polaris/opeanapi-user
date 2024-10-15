d3bb7ea9fe7b9c2d7a5c9a6fa694cd2e1e36711c
univero <1449610641@qq.com>
2024-10-15 14:59:04 +0800

    fix: 将函数签名中大写的Req全改为小写的req

--- biz/adaptor/controller/auth.go
+++ biz/adaptor/controller/auth.go
@@ -8,15 +8,15 @@ import (
 )
 
 type IAuthController interface {
-	SignUp(ctx context.Context, Req *user.SignUpReq) (r *user.SignUpResp, err error)
-	GetUserInfo(ctx context.Context, Req *user.GetUserInfoReq) (r *user.GetUserInfoResp, err error)
-	SetUserInfo(ctx context.Context, Req *user.SetUserInfoReq) (r *user.SetUserInfoResp, err error)
-	CreateKey(ctx context.Context, Req *user.CreateKeyReq) (r *user.CreateKeyResp, err error)
-	GetKey(ctx context.Context, Req *user.GetKeysReq) (r *user.GetKeysResp, err error)
-	UpdateKey(ctx context.Context, Req *user.UpdateKeyReq) (r *user.UpdateKeyResp, err error)
-	UpdateHosts(ctx context.Context, Req *user.UpdateHostsReq) (r *user.UpdateHostsResp, err error)
-	RefreshKey(ctx context.Context, Req *user.RefreshKeyReq) (r *user.RefreshKeyResp, err error)
-	DeleteKey(ctx context.Context, Req *user.DeleteKeyReq) (r *user.DeleteKeyResp, err error)
+	SignUp(ctx context.Context, req *user.SignUpReq) (r *user.SignUpResp, err error)
+	GetUserInfo(ctx context.Context, req *user.GetUserInfoReq) (r *user.GetUserInfoResp, err error)
+	SetUserInfo(ctx context.Context, req *user.SetUserInfoReq) (r *user.SetUserInfoResp, err error)
+	CreateKey(ctx context.Context, req *user.CreateKeyReq) (r *user.CreateKeyResp, err error)
+	GetKey(ctx context.Context, req *user.GetKeysReq) (r *user.GetKeysResp, err error)
+	UpdateKey(ctx context.Context, req *user.UpdateKeyReq) (r *user.UpdateKeyResp, err error)
+	UpdateHosts(ctx context.Context, req *user.UpdateHostsReq) (r *user.UpdateHostsResp, err error)
+	RefreshKey(ctx context.Context, req *user.RefreshKeyReq) (r *user.RefreshKeyResp, err error)
+	DeleteKey(ctx context.Context, req *user.DeleteKeyReq) (r *user.DeleteKeyResp, err error)
 }
 
 type AuthController struct {
@@ -29,38 +29,38 @@ var AuthControllerSet = wire.NewSet(
 	wire.Bind(new(IAuthController), new(*AuthController)),
 )
 
-func (c *AuthController) SignUp(ctx context.Context, Req *user.SignUpReq) (r *user.SignUpResp, err error) {
-	return c.UserService.SignUp(ctx, Req)
+func (c *AuthController) SignUp(ctx context.Context, req *user.SignUpReq) (r *user.SignUpResp, err error) {
+	return c.UserService.SignUp(ctx, req)
 }
 
-func (c *AuthController) GetUserInfo(ctx context.Context, Req *user.GetUserInfoReq) (r *user.GetUserInfoResp, err error) {
-	return c.UserService.GetUserInfo(ctx, Req)
+func (c *AuthController) GetUserInfo(ctx context.Context, req *user.GetUserInfoReq) (r *user.GetUserInfoResp, err error) {
+	return c.UserService.GetUserInfo(ctx, req)
 }
 
-func (c *AuthController) SetUserInfo(ctx context.Context, Req *user.SetUserInfoReq) (r *user.SetUserInfoResp, err error) {
-	return c.UserService.SetUserInfo(ctx, Req)
+func (c *AuthController) SetUserInfo(ctx context.Context, req *user.SetUserInfoReq) (r *user.SetUserInfoResp, err error) {
+	return c.UserService.SetUserInfo(ctx, req)
 }
 
-func (c *AuthController) CreateKey(ctx context.Context, Req *user.CreateKeyReq) (r *user.CreateKeyResp, err error) {
-	return c.KeyService.CreateKey(ctx, Req)
+func (c *AuthController) CreateKey(ctx context.Context, req *user.CreateKeyReq) (r *user.CreateKeyResp, err error) {
+	return c.KeyService.CreateKey(ctx, req)
 }
 
-func (c *AuthController) GetKey(ctx context.Context, Req *user.GetKeysReq) (r *user.GetKeysResp, err error) {
-	return c.KeyService.GetKey(ctx, Req)
+func (c *AuthController) GetKey(ctx context.Context, req *user.GetKeysReq) (r *user.GetKeysResp, err error) {
+	return c.KeyService.GetKey(ctx, req)
 }
 
-func (c *AuthController) UpdateKey(ctx context.Context, Req *user.UpdateKeyReq) (r *user.UpdateKeyResp, err error) {
-	return c.KeyService.UpdateKey(ctx, Req)
+func (c *AuthController) UpdateKey(ctx context.Context, req *user.UpdateKeyReq) (r *user.UpdateKeyResp, err error) {
+	return c.KeyService.UpdateKey(ctx, req)
 }
 
-func (c *AuthController) UpdateHosts(ctx context.Context, Req *user.UpdateHostsReq) (r *user.UpdateHostsResp, err error) {
-	return c.KeyService.UpdateHosts(ctx, Req)
+func (c *AuthController) UpdateHosts(ctx context.Context, req *user.UpdateHostsReq) (r *user.UpdateHostsResp, err error) {
+	return c.KeyService.UpdateHosts(ctx, req)
 }
 
-func (c *AuthController) RefreshKey(ctx context.Context, Req *user.RefreshKeyReq) (r *user.RefreshKeyResp, err error) {
-	return c.KeyService.RefreshKey(ctx, Req)
+func (c *AuthController) RefreshKey(ctx context.Context, req *user.RefreshKeyReq) (r *user.RefreshKeyResp, err error) {
+	return c.KeyService.RefreshKey(ctx, req)
 }
 
-func (c *AuthController) DeleteKey(ctx context.Context, Req *user.DeleteKeyReq) (r *user.DeleteKeyResp, err error) {
-	return c.KeyService.DeleteKey(ctx, Req)
+func (c *AuthController) DeleteKey(ctx context.Context, req *user.DeleteKeyReq) (r *user.DeleteKeyResp, err error) {
+	return c.KeyService.DeleteKey(ctx, req)
 }
